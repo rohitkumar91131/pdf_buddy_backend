0f9f20efb834a99777fe8b033ff850f56e1ee108
rohitkumar91131 <rk34190100@gmail.com>
2025-09-10 04:09:31 +0530

    added condition to create uploads folder if not present

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ dotenv.config();
 import "./config/db.js";
 import multer from 'multer';
 import express from "express";
-
+import fs from 'fs'
 import authRoutes from "./routes/AuthRoutes.js";
 import pdfRoutes from './routes/PdfRoutes.js';
 import cors from 'cors';
@@ -23,6 +23,10 @@ app.use(cookieParser());
 app.use("/auth", authRoutes);
 app.use("/pdfs", pdfRoutes);
 
+
+if(!fs.existsSync("uploads")){
+  fs.mkdirSync("uploads")
+}
 app.listen(PORT, () => {
     console.log("Frontend URL:", process.env.FRONTEND_URL);
     console.log(`🚀 Server running on port ${PORT}`);
